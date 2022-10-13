f65b2a0e7969647bbd49bb6255631df464ef7291
Ktari ayman <aymanktari31@gmail.com>
2022-10-13 20:39:58 +0100

    register version 0  with validation without db

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import { RegisterValidation } from "../validation/registerValidation";
 
 export const Register = (req:Request, res:Response) => {
+    const body = req.body;
+    const {error} = RegisterValidation.validate(body)
+    if(error){
+        return res.status(404).send(error.details);
+    }
+    if(body.password !== body.password_confirm){
+        return res.status(404).send({
+            message:"password does not match"
+        });
+    }
     res.send(req.body);
 }
