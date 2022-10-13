fbd7a3367e61bb9006ee30a8a06a1f8f6599b05c
Ktari ayman <aymanktari31@gmail.com>
2022-10-13 22:56:55 +0100

    install jwt ,  bcryptjs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, { Request , Response} from 'express';
 import cors from "cors";
 import { routes } from './routes';
-const app = express();
-app.use(express.json())
-app.use(cors({
-    origin :["http://localhost:3000"]
-}))
-routes(app)
-app.listen(8000, ()=> {
-    console.log('hello there and welcome , Port = 8000')
-})
+import { createConnection } from "typeorm"
+
+createConnection()
+    .then((Connection) => {
+        const app = express();
+        app.use(express.json())
+        app.use(cors({
+            credentials :true, // for exemple front end can get the cookies
+            origin :["http://localhost:3000"]
+        }))
+        routes(app)
+        app.listen(8000, ()=> {
+            console.log('hello there and welcome , Port = 8000')
+        })    })
+    .catch((error) => console.log(error))
+
