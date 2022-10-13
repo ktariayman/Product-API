986a118c91f389b38da8586c502eb59bacbbbafa
Ktari ayman <aymanktari31@gmail.com>
2022-10-13 22:57:06 +0100

    add login to the route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { Register } from "./controller/auth";
+import { Login, Register } from "./controller/auth";
 
 export const routes = (router :Router) => {
     router.post('/api/register' , Register)
+    router.post('/api/login' , Login)
 }
