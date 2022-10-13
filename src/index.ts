603361ac542e95145881df565364619d33219e3d
Ktari ayman <aymanktari31@gmail.com>
2022-10-13 19:13:14 +0100

    add Routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { Request , Response} from 'express';
 import cors from "cors";
+import { routes } from './routes';
 const app = express();
 app.use(express.json())
-app.use(cors)
+app.use(cors({
+    origin :["http://localhost:3000"]
+}))
+routes(app)
 app.listen(8000, ()=> {
     console.log('hello there and welcome , Port = 8000')
-})
-app.get('/', (req:Request,res:Response) =>{
-    res.send("hello world")
 })
