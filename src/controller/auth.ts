20e3f08d2ecebeb3f1e46bd3f1cb22923b05d032
Ktari ayman <aymanktari31@gmail.com>
2022-10-13 22:57:54 +0100

    registration and login functionality

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 import { RegisterValidation } from "../validation/registerValidation";
+import {getManager} from "typeorm"
+import { User } from "../entity/user";
+import bcryptjs from "bcryptjs";
+import jsonwebtoken from 'jsonwebtoken';
+const { sign, decode, verify } = jsonwebtoken;
 
-export const Register = (req:Request, res:Response) => {
+export const Register = async (req:Request, res:Response) => {
     const body = req.body;
     const {error} = RegisterValidation.validate(body)
     if(error){
@@ -12,5 +17,43 @@ export const Register = (req:Request, res:Response) => {
             message:"password does not match"
         });
     }
-    res.send(req.body);
+    const repository = getManager().getRepository(User)
+    const {password , ...user} = await repository.save({
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        password : await bcryptjs.hash(body.password , 10)
+    })
+    res.send(user);
+}
+
+export const Login = async (req:Request, res:Response) => {
+    const repository = getManager().getRepository(User)
+    const user =  await repository.findOne(
+        { where:
+            { email: req.body.email }
+        }
+    )
+    if(!user){
+
+        return res.status(404).send({
+            message:"invalid credentials",
+        })
+    }
+    const samePassword = await bcryptjs.compare( req.body.password,user.password)
+    if(!samePassword){
+        return res.status(404).send({
+            message:"invalid credentials",
+        })
+    }
+    const payload = {
+        id : user.id,
+    }
+    const token = sign(payload , "secret");
+    res.cookie('jwt' , token , {
+        httpOnly: true,
+        maxAge: 30*24*60*60*3600 // 30days
+    })
+    const {password , ...userWithoutPassword}= user
+    res.send("success" )
 }
