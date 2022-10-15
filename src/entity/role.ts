18bb788cc7253195f1176b5231b66ea270f83b8f
Ktari ayman <aymanktari31@gmail.com>
2022-10-15 18:39:07 +0100

    add permission

--- src/entity/role.ts
+++ src/entity/role.ts
@@ -1,4 +1,5 @@
-import { Entity , PrimaryGeneratedColumn , Column} from "typeorm";
+import { Entity , PrimaryGeneratedColumn , Column, ManyToMany, JoinTable} from "typeorm";
+import { Permission } from "./Permission";
 
 @Entity()
 export class Role {
@@ -7,4 +8,12 @@ export class Role {
     
     @Column()
     name: string;;
+
+    @ManyToMany ( () => Permission)
+    @JoinTable({
+        name: "role_permission",
+        joinColumn: {name : "role_id" , referencedColumnName: "id"},
+        inverseJoinColumn: {name : "permission_id" , referencedColumnName: "id"},
+    })
+    permissions : Permission[]
 }
