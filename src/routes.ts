7b03489f6c2717fd143603dfc4c6ebda97671028
Ktari ayman <aymanktari31@gmail.com>
2022-10-15 09:53:12 +0100

    users CRUD : add endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { authenticatedUser, Login, logout, Register, UpdateInfo, UpdatePassword } from "./controller/auth";
+import { createUser, deleteUser, getAllUsers, getUser, updateUser } from "./controller/users";
 import { authMiddleware } from "./middleware/auth";
 
 export const routes = (router :Router) => {
@@ -9,4 +10,11 @@ export const routes = (router :Router) => {
     router.post('/api/logout' ,authMiddleware, logout)
     router.put('/api/users/info' ,authMiddleware, UpdateInfo)
     router.put('/api/users/password' ,authMiddleware, UpdatePassword)
+
+    //user Controller
+    router.get('/api/users' ,authMiddleware, getAllUsers)
+    router.get('/api/users/:id' ,authMiddleware, getUser)
+    router.post('/api/users' ,authMiddleware, createUser)
+    router.put('/api/users/:id' ,authMiddleware, updateUser)
+    router.delete('/api/users/:id' ,authMiddleware, deleteUser)
 }
