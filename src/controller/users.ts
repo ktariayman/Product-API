dd78dd42c609be854f114caeb1ef2926744ef780
Ktari ayman <aymanktari31@gmail.com>
2022-10-15 11:28:21 +0100

    add role to users CRUD

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -5,7 +5,9 @@ import bcryptjs from "bcryptjs";
 
 export const getAllUsers = async (req:Request, res:Response) => {
     const repository = getManager().getRepository(User)
-    const users = await repository.find()
+    const users = await repository.find({
+        relations :["role"]
+    })
     const usersWithourPassword = users.map(u => {
         const {password , ...data} = u
         return data
@@ -18,10 +20,23 @@ export const createUser = async (req:Request, res:Response) => {
     const hashedpassword = await bcryptjs.hash("1234" , 10)
 
     const repository = getManager().getRepository(User)
+    const userExist =  await repository.findOne(
+        { where:
+            { email: req.body.email }
+        }
+    )
+    if(userExist){
 
+        return res.status(404).send({
+            message:"user already exists",
+        })
+    }
     const user = await repository.save({
         ...body,
         password:hashedpassword,
+        role: {
+            id:role_id,
+        }
     })
     const {password , ...userWithoutPassword} = user
     res.send(userWithoutPassword)
@@ -29,7 +44,11 @@ export const createUser = async (req:Request, res:Response) => {
 
 export const getUser = async (req:Request, res:Response) => {
     const repository = getManager().getRepository(User)
-    const user = await repository.findOne(    {where: {id: parseInt(req.params.id, 10)}});
+    const user = await repository.findOne(    { 
+        relations: {
+        role    :true ,
+    },
+        where: {id: parseInt(req.params.id, 10)}}  );
     if(!user){
 
         return res.status(404).send({
@@ -39,19 +58,44 @@ export const getUser = async (req:Request, res:Response) => {
     const {password , ...userWithoutPassword} = user
     res.send(userWithoutPassword)
 }
+
+
 export const updateUser = async (req:Request, res:Response) => {
     const {role_id , ...body} = req.body 
-
     const repository = getManager().getRepository(User)
-    const user = await repository.findOne({where: {id: parseInt(req.params.id, 10)}});
+    const user = await repository.findOne(
+        { 
+            relations: {
+            role    :true ,
+        },
+            where: {id: parseInt(req.params.id, 10)}}
+    );
     if(!user){
         return res.status(404).send({
             message:`no user with this is ${req.params.id}`,
         })
     }
-    await repository.update(req.params.id,body)
-    const updatedUser =  await repository.findOne({where: {id: parseInt(req.params.id, 10)}});
-    res.send(updatedUser)
+    await repository.update(req.params.id,{
+        ...body,
+        role:{
+            id: role_id,
+        }
+    })
+    const updatedUser =  await repository.findOne(
+        { 
+            relations: {
+            role    :true ,
+        },
+            where: {id: parseInt(req.params.id, 10)}}
+    );
+    if(!updatedUser){
+        return res.status(404).send({
+            message:`no user has been updated with this is ${req.params.id}`,
+        })
+    }
+    const {password , ...userWithoutPassword} = updatedUser
+
+    res.send(userWithoutPassword)
 }
 
 
