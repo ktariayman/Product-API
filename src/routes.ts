7b3e9a0bdbca1b8edfe51bc1f5967d3603aaa384
Ktari ayman <aymanktari31@gmail.com>
2022-10-17 09:11:26 +0100

    fix

--- src/routes.ts
+++ src/routes.ts
@@ -9,8 +9,11 @@ import { createProduct, deleteProduct, getAllProducts, getProduct, updateProduct
 import { createRole, deleteRole, getAllRoles, getRole, updateRole } from "./controller/role";
 import { createUser, deleteUser, getAllUsers, getUser, updateUser } from "./controller/users";
 import { authMiddleware } from "./middleware/auth";
+import { getAllOrders } from "./controller/order";
 
 export const routes = (router :Router) => {
+    // auth Controller
+    
     router.post('/api/register' , Register)
     router.post('/api/login' , Login)
     router.get('/api/user' , authMiddleware,authenticatedUser)
@@ -19,6 +22,7 @@ export const routes = (router :Router) => {
     router.put('/api/users/password' ,authMiddleware, UpdatePassword)
 
     //user Controller
+    
     router.get('/api/users' ,authMiddleware, getAllUsers)
     router.get('/api/users/:id' ,authMiddleware, getUser)
     router.post('/api/users' ,authMiddleware, createUser)
@@ -26,20 +30,28 @@ export const routes = (router :Router) => {
     router.delete('/api/users/:id' ,authMiddleware, deleteUser)
 
     router.get('/api/permissions' ,authMiddleware, Permissions)
+    
+    //roles Controller
 
     router.get('/api/roles' ,authMiddleware, getAllRoles)
     router.get('/api/roles/:id' ,authMiddleware, getRole)
     router.post('/api/roles' ,authMiddleware, createRole)
     router.put('/api/roles/:id' ,authMiddleware, updateRole)
     router.delete('/api/roles/:id' ,authMiddleware, deleteRole)
+    
+    //products Controller
 
     router.get('/api/products' ,authMiddleware, getAllProducts)
     router.get('/api/roles/:id' ,authMiddleware, getProduct)
     router.post('/api/roles' ,authMiddleware, createProduct)
     router.put('/api/roles/:id' ,authMiddleware, updateProduct)
     router.delete('/api/roles/:id' ,authMiddleware, deleteProduct)
-
+    
+    //upload Controller
+    
     router.post('/api/upload' ,authMiddleware, upload)
     router.use('/api/upload' ,express.static("./uploads"))
+
+    router.get('/api/orders' ,authMiddleware, getAllOrders)
 
 }
